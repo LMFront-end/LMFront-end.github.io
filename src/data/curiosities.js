7839b48e989470fe0da509226008a249b02a89cf
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-27 09:53:57 -0500

    Se termina data de curiosities

--- src/data/curiosities.js
+++ src/data/curiosities.js
@@ -14,50 +14,50 @@ const curiosities = [
     },
     {
         slug: "icuriosity-3",
-        name: "name",
-        photo: "foto",
-        description: "descripcion"
+        name: "Rick's burps more realistic",
+        photo: "https://pbs.twimg.com/media/D3P8IR9WwAE3G0B.jpg",
+        description: "To make Rick's burps more realistic, the people in charge of the series record them as naturally as possible in front of the microphone: they drink liters of mineral water, low-calorie beer and carbonated beverages before entering the studio."
     },
     {
         slug: "curiosity-4",
-        name: "name",
-        photo: "foto",
-        description: "descripcion"
+        name: "Inspirations from other series",
+        photo: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2017/11/simpson-homenajean-rick-morty-proximo-episodio.png?itok=wLahlc2j",
+        description: "Inspirations from other series: the creators of Rick and Morty are fans of other series such as Ren and Stimpy, South Park and Beavis and Butthead, among others.  This can be noticed, in addition to the type of humor, in the color style. "
     },
     {
         slug: "curiosity-5",
-        name: "name",
-        photo: "foto",
-        description: "descripcion"
+        name: "tribute to Breaking Bad",
+        photo: "https://c.tenor.com/1EH3zPpQ7zUAAAAC/rick-and-morty-breaking-bad.gif",
+        description: "The creators of the series wanted to pay a small tribute to the successful Breaking Bad. And they did it during the chapter in which Rick remembers the supposed death of his wife. If you look closely, you will notice that the house is practically the same as Walter White's."
     },
     {
         slug: "curiosity-6",
-        name: "name",
-        photo: "foto",
-        description: "descripcion"
+        name: "9 out of 10 on IMDb",
+        photo: "https://www.wazomagazine.com/wp-content/uploads/2018/05/Eres-fan-de-Rick-y-Morty-Conoce-al-compositor-de-su-banda-sonora-2.jpg",
+        description: "Rick and Morty is the only animated series that has more than a 9 out of 10 on IMDb. An achievement that, at least in these terms, places it above other proposals such as Adventure Time, Family Guy, South Park or The Simpsons itself. Big words.  "
     },
     {
         slug: "curiosity-7",
-        name: "name",
-        photo: "foto",
-        description: "descripcion"
+        name: "the aliens ",
+        photo: "https://i.blogs.es/a9f139/solaropposites-/840_560.jpeg",
+        description: "Each and every one of the aliens that appear in 'Rick and Morty' are created based on scatological elements such as genitals or feces, a fact that has been confessed by the creators themselves. "
     },
     {
         slug: "curiosity-8",
-        name: "name",
-        photo: "foto",
-        description: "descripcion"
+        name: "Wubba Lubba Dub Dub!",
+        photo: "https://img.rationalcdn.com/starsweb/prod/10810_0.jpg",
+        description: "In the episode Meeseeks Destructors is where Rick invents his slogan 'Wubba Lubba Dub Dub!'. This episode shows some of Rick's affection for Morty by killing King Jellybean for his actions against him."
     },
     {
         slug: "curiosity-9",
-        name: "name",
-        photo: "foto",
-        description: "descripcion"
+        name: "Rick and Morty are the only two original characters",
+        photo: "https://www.geekmi.news/__export/1626192795835/sites/debate/img/2021/07/13/disexo_x7x.jpg_242310155.jpg",
+        description: "As of the episode Rick's Potion, Rick and Morty are technically the only two original characters who appear regularly in the series, as they live the rest of the series in a clone reality."
     },
     {
         slug: "curiosity-10",
-        name: "name",
-        photo: "foto",
-        description: "descripcion"
+        name: "Jerry is famous",
+        photo: "https://gcdn.lanetaneta.com/wp-content/uploads/2019/12/Rick-amp-Morty-el-arco-de-redenci%C3%B3n-de-Jerry-Smith.jpg",
+        description: "In episode 8 of the first season, the reality in which Jerry is famous, all the movies Jerry appears in are real-life movies in which he has taken the place of all the characters played by Tom Hanks."
     },
 ]
