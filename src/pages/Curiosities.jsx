3a29bf064d03a850a578de2ca687a9ccb17f0a1a
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-27 04:04:18 -0500

    Se crea curiosities services

--- src/pages/Curiosities.jsx
+++ src/pages/Curiosities.jsx
@@ -12,9 +12,9 @@ const Curiosities = () => {
                 <article className="projects__item">
                     <img src="https://i.pinimg.com/originals/80/75/a0/8075a0efc5c6fbc56c6d9ebd215bd87f.gif" class="projects__img" />
                     <div className="projects__hover">
-                        <p className="projects__title">
+                        <h2 className="projects__title">
                             Rick Sanchez
-                        </p>
+                        </h2>
                         <i className="far fa-file-alt projects__icon"></i>
                     </div>
                 </article>
@@ -23,7 +23,7 @@ const Curiosities = () => {
                 <article className="projects__item">
                     <img src="https://c.tenor.com/4y2cEyMvCigAAAAC/morty-smith-morty-cry.gif" className="projects__img" />
                     <div className="projects__hover">
-                        <h2 className="projects__title">Katie B.</h2>
+                        <h2 className="projects__title">Morty Smith</h2>
                         <i className="far fa-file-alt projects__icon"></i>
                     </div>
                 </article>
@@ -32,7 +32,7 @@ const Curiosities = () => {
                 <article className="projects__item">
                     <img src="https://data.whicdn.com/images/304151549/original.gif" class="projects__img" />
                     <div className="projects__hover">
-                        <h2 className="projects__title">Margaret H.</h2>
+                        <h2 className="projects__title">Summer Smith</h2>
                         <i className="far fa-file-alt projects__icon"></i>
                     </div>
                 </article>
@@ -41,7 +41,7 @@ const Curiosities = () => {
                 <article className="projects__item">
                     <img src="https://pa1.narvii.com/6540/1a0fc4767b2457d1cdb4a33ea9fd7704b7b01d87_hq.gif" class="projects__img" />
                     <div className="projects__hover">
-                        <h2 className="projects__title">An E.R.A.</h2>
+                        <h2 className="projects__title">Jerry Smith</h2>
                         <i className="far fa-file-alt projects__icon"></i>
                     </div>
                 </article>
@@ -50,7 +50,7 @@ const Curiosities = () => {
                 <article className="projects__item">
                     <img src="https://thumbs.gfycat.com/AggravatingAliveAnemonecrab-max-1mb.gif" className="projects__img" />
                     <div className="projects__hover">
-                        <h2 className="projects__title">Computers</h2>
+                        <h2 className="projects__title">Beth Smith</h2>
                         <i className="far fa-file-alt projects__icon"></i>
                     </div>
                 </article>
@@ -59,7 +59,7 @@ const Curiosities = () => {
                 <article className="projects__item">
                     <img src="https://c.tenor.com/g8stX4vRnxkAAAAC/meeseeks-rick.gif" className="projects__img" />
                     <div className="projects__hover">
-                        <h2 className="projects__title">Susan K.</h2>
+                        <h2 className="projects__title">mr meeseek</h2>
                         <i className="far fa-file-alt projects__icon"></i>
                     </div>
                 </article>
