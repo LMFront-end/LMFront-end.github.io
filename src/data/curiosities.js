07256ae76895ea6a090dad7fed6695011bc42935
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-27 09:41:37 -0500

    Se crea data de curiosities

---
+++ src/data/curiosities.js
@@ -0,0 +1,63 @@
+const curiosities = [
+
+    {
+        slug: "curiosity-1",
+        name: "parody of Back to the Future",
+        photo: "https://media.vandalsports.com/i/640x360/3-2022/20223292845_1.jpg",
+        description: "Rick and Morty grew out of a parody of Back to the Future: Rick and Morty derives from a creation of Justin Roiland himself, a web series called The Real Animated Adventures of Doc and Mharti. We remember that the Back to the Future characters were called Doc and Marty. "
+    },
+    {
+        slug: "curiosity-2",
+        name: "pilot episode of 'Rick and Morty'",
+        photo: "https://www.cinemascomics.com/wp-content/uploads/2021/12/rick-y-morty-navidad-video-rap.jpg",
+        description: "The exact time it took Dan Harmon and Justin Roiland to write and shape the pilot episode of 'Rick and Morty' was six hours. Not one more, not one less. In fact, the development of this episode took place the same day they managed to sell the project to the studio, which added even more pressure. "
+    },
+    {
+        slug: "icuriosity-3",
+        name: "name",
+        photo: "foto",
+        description: "descripcion"
+    },
+    {
+        slug: "curiosity-4",
+        name: "name",
+        photo: "foto",
+        description: "descripcion"
+    },
+    {
+        slug: "curiosity-5",
+        name: "name",
+        photo: "foto",
+        description: "descripcion"
+    },
+    {
+        slug: "curiosity-6",
+        name: "name",
+        photo: "foto",
+        description: "descripcion"
+    },
+    {
+        slug: "curiosity-7",
+        name: "name",
+        photo: "foto",
+        description: "descripcion"
+    },
+    {
+        slug: "curiosity-8",
+        name: "name",
+        photo: "foto",
+        description: "descripcion"
+    },
+    {
+        slug: "curiosity-9",
+        name: "name",
+        photo: "foto",
+        description: "descripcion"
+    },
+    {
+        slug: "curiosity-10",
+        name: "name",
+        photo: "foto",
+        description: "descripcion"
+    },
+]
