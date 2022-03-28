a86455b82dda38eb9cacc739b4cf7750fadab954
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-27 20:58:01 -0500

    Se importa provider

--- src/main.jsx
+++ src/main.jsx
@@ -12,14 +12,17 @@ import '../node_modules/react-icons/fa';
 
 import {BrowserRouter, Route, Routes} from '../node_modules/react-router-dom';
 import {Provider} from 'react-redux';
+import {store} from './features/store';
 
 
 
 ReactDOM.render(
   <BrowserRouter>
+  <Provider store={store}>
     <Routes>
       <Route path="*" element={<App />} />  
     </Routes> 
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 )
